50d12b655b8ac82b163e431125362144cf1ff1be
thuanpham582002 <95533596+thuanpham582002@users.noreply.github.com>
2023-10-17 09:11:01 +0700

    first commit

---
+++ app/src/main/java/dev/thuan/util/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package dev.thuan.util.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
